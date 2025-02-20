236744fb5788e07542b2da601f29f4cf95d7cbb9
Hayssam Saleh <hayssam@saleh.fr>
2025-02-20 15:54:46 +0100

    Handle transform. Audit and expectations logging still missing

--- notebooks/transform.py
+++ notebooks/transform.py
@@ -3,6 +3,7 @@ from snowflake.snowpark import DataFrame
 from snowflake.snowpark.row import Row
 import pandas as pd
 from typing import List, Optional, Tuple, Union
+import os
 
 connection_parameters = {
    "account": os.environ['SNOWFLAKE_ACCOUNT'],
@@ -13,6 +14,64 @@ connection_parameters = {
 #   "schema": "<your snowflake schema>",  # optional
    "database": os.environ['SNOWFLAKE_DB'],  # optional
 }
+
+
+
+expectations = {
+  "kpi.order_items_analysis" : [ {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.order_items_analysis)\nSELECT COALESCE(max(cnt), 0)\n    FROM (SELECT , count(*) as cnt FROM sl_this GROUP BY ) AS COL_COUNT",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.order_items_analysis)\nselect\n    case\n    when count(*) between 1 and 2 then 1\n    else 0\n    end\n    from SL_THIS",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.order_items_analysis)\nSELECT COALESCE(max(cnt), 0)\n    FROM (SELECT , count(*) as cnt FROM sl_this GROUP BY ) AS COL_COUNT",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.order_items_analysis)\nselect\n    case\n    when count(*) between 1 and 2 then 1\n    else 0\n    end\n    from SL_THIS",
+    "failOnError" : "no"
+  } ],
+  "kpi.order_summary" : [ {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.order_summary)\nSELECT COALESCE(max(cnt), 0)\n    FROM (SELECT , count(*) as cnt FROM sl_this GROUP BY ) AS COL_COUNT",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.order_summary)\nselect\n    case\n    when count(*) between 1 and 2 then 1\n    else 0\n    end\n    from SL_THIS",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.order_summary)\nSELECT COALESCE(max(cnt), 0)\n    FROM (SELECT , count(*) as cnt FROM sl_this GROUP BY ) AS COL_COUNT",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.order_summary)\nselect\n    case\n    when count(*) between 1 and 2 then 1\n    else 0\n    end\n    from SL_THIS",
+    "failOnError" : "no"
+  } ],
+  "kpi.product_summary" : [ {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.product_summary)\nSELECT COALESCE(max(cnt), 0)\n    FROM (SELECT , count(*) as cnt FROM sl_this GROUP BY ) AS COL_COUNT",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.product_summary)\nselect\n    case\n    when count(*) between 1 and 2 then 1\n    else 0\n    end\n    from SL_THIS",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.product_summary)\nSELECT COALESCE(max(cnt), 0)\n    FROM (SELECT , count(*) as cnt FROM sl_this GROUP BY ) AS COL_COUNT",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.product_summary)\nselect\n    case\n    when count(*) between 1 and 2 then 1\n    else 0\n    end\n    from SL_THIS",
+    "failOnError" : "no"
+  } ],
+  "kpi.revenue_summary" : [ {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.revenue_summary)\nSELECT COALESCE(max(cnt), 0)\n    FROM (SELECT , count(*) as cnt FROM sl_this GROUP BY ) AS COL_COUNT",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.revenue_summary)\nselect\n    case\n    when count(*) between 1 and 2 then 1\n    else 0\n    end\n    from SL_THIS",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.revenue_summary)\nSELECT COALESCE(max(cnt), 0)\n    FROM (SELECT , count(*) as cnt FROM sl_this GROUP BY ) AS COL_COUNT",
+    "failOnError" : "no"
+  }, {
+    "expect" : "WITH SL_THIS AS (SELECT * FROM kpi.revenue_summary)\nselect\n    case\n    when count(*) between 1 and 2 then 1\n    else 0\n    end\n    from SL_THIS",
+    "failOnError" : "no"
+  } ]
+}
+
 
 audit = {
   "preActions" : [ "USE SCHEMA audit" ],
@@ -45,6 +104,12 @@ statements = {
   }
 }
 
+expectations = {
+  "createSchemaSql" : [ "CREATE TABLE IF NOT EXISTS audit.expectations (\n                            JOBID VARCHAR NOT NULL,\n                            DATABASE VARCHAR,\n                            DOMAIN VARCHAR NOT NULL,\n                            SCHEMA VARCHAR NOT NULL,\n                            TIMESTAMP TIMESTAMP NOT NULL,\n                            NAME VARCHAR NOT NULL,\n                            PARAMS VARCHAR NOT NULL,\n                            SQL VARCHAR NOT NULL,\n                            COUNT BIGINT NOT NULL,\n                            EXCEPTION VARCHAR NOT NULL,\n                            SUCCESS BOOLEAN NOT NULL\n                          )\n        " ],
+  "mainSqlIfExists" : [ "\n          SELECT\n            '{{jobid}}' AS JOBID,\n            '{{database}}' AS DATABASE,\n            '{{domain}}' AS DOMAIN,\n            '{{schema}}' AS SCHEMA,\n            TO_TIMESTAMP('{{timestamp}}') AS TIMESTAMP,\n            '{{name}}' AS NAME,\n            '{{params}}' AS PARAMS,\n            '{{sql}}' AS SQL,\n            {{count}} AS COUNT,\n            '{{exception}}' AS EXCEPTION,\n            {{success}} AS SUCCESS\n        " ]
+}
+
+acl = {}
 
 def sl_main(session: Session, task: str): 
    for statement in statements.get(task, {}).get("preActions", []):
@@ -54,12 +119,12 @@ def sl_main(session: Session, task: str):
       print(f'{task} table does not exist')
       sqls: List[str] = statements.get(task, {}).get('mainSqlIfNotExists', [])
       for sql in sqls:
-            session.sql(sql).show()
+            session.sql(sql)
    else:
       print(f'{task} table exists')
       sqls: List[str] = statements.get(task, {}).get('mainSqlIfExists', [])
       for sql in sqls:
-            session.sql(sql).show()
+            session.sql(sql)
 
 def sl_table_exists(session: Session, table_name: str) -> bool:
     df: DataFrame = session.sql(query=f"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE CONCAT(TABLE_SCHEMA, '.', TABLE_NAME) ILIKE '{table_name}'") 
@@ -75,7 +140,22 @@ def sl_create_audit_table(session: Session):
       print('AUDIT table does not exist')
       sqls: List[str] = audit.get('createSchemaSql', [])
       for sql in sqls:
-            session.sql(sql).show()
+            session.sql(sql)
+
+def sl_create_expectations_table(session: Session):
+   sl_expectation_table_exists = sl_table_exists(session, 'audit.expectations')
+   if  not sl_expectation_table_exists:
+      print('EXPECTATIONS table does not exist')
+      sqls: List[str] = expectations.get('createSchemaSql', [])
+      for sql in sqls:
+            session.sql(sql)
+
+
+def sl_create_acl(session: Session, task: str):
+    if acl.get(task, None) is not None:
+      sqls: List[str] = acl.get(task)
+      for sql in sqls:
+         session.sql(sql)
 
 
 
@@ -83,10 +163,43 @@ session = Session.builder.configs(connection_parameters).create()
 
 def sl_run_task(session: Session, task: str):
    try:
+      session.sql("BEGIN")
       sl_create_audit_table(session)
       sl_main(session, task)
+      session.sql("END")
    except Exception as e:
+      session.sql("ROLLBACK")
       error_message = str(e)
       print(error_message)
-   finally:
-      session.close()
+      raise e
+
+
+def sl_run_expectations(session: Session, task: str):
+   for expectation in expectations.get(task, []):
+      print(expectation)
+      expect = expectation.get("expect")
+      failOnError = expectation.get("failOnError")
+      count = 0
+      try:
+         df: DataFrame = session.sql(expect)
+         rows: List[Row] = df.collect()
+         if rows.__len__ != 1:
+            raise Exception(f'Expectation failed for {task}: {expect}. Expected 1 row but got {rows.__len__()}')
+         count = rows.collect()[0]("cnt")
+         # todo log expectations in expectation table here
+         if count != 0:
+            raise Exception(f'Expectation failed for {task}: {expect}. Expected count to be equal to 0 but got {count}')
+      except Exception as e:
+         error_message = str(e)
+         print(error_message)
+         if failOnError == "yes":
+            raise e
+
+
+def sl_main():
+   sl_create_audit_table(session)
+   sl_create_expectations_table(session)
+   for task in statements.keys():
+      sl_run_task(session, task)
+      sl_run_expectations(session, task)
+
