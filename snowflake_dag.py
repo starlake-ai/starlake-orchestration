c978e14f144bc888d1866ac06a19f6219f4b6c42
Stéphane Manciot <stephane.manciot@gmail.com>
2025-08-13 16:42:35 +0300

    rename run_dependencies to run_dependencies_first

--- snowflake_dag.py
+++ snowflake_dag.py
@@ -13,7 +13,7 @@ access_control = None
 options={
     'sl_env_var':'{"SL_ROOT": ".", "SL_ENV": "SNOWFLAKE"}', 
     'tags':'starlake', 
-    'run_dependencies':'false', 
+    'run_dependencies_first':'false', 
     'retries':'1',
     'retry_delay':'30',
     'stage_location':'staging',
