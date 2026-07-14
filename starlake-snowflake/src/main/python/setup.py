e6fe7aad374ffacc967bc9b6de7f3d0da902ac4a
Stéphane Manciot <stephane.manciot@gmail.com>
2026-07-14 20:31:23 +0200

    chore: bump versions for starlake packages to 0.6.1, 0.5.1, and 0.4.1 (sl_resolve_pre_load_strategy added)

--- starlake-snowflake/src/main/python/setup.py
+++ starlake-snowflake/src/main/python/setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 import os
 
-version = os.environ.get("PROJECT_VERSION", "0.4.0")
+version = os.environ.get("PROJECT_VERSION", "0.4.1")
 
 setup(name='starlake-snowflake',
       version=version,
